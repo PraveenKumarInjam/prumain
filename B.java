2b07223391502763eea639ed238fd07c8acf163b
NeeharicaPallapati <neeharica.pallapati@gmail.com>
2022-05-26 11:43:14 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,11 @@
+import Sample1.A;
+import Sample3.C;
+
+public class B {
+public static void main(String args[]){
+A a = new A();
+System.out.println("Addition: " + a.add(3, 5));
+C c= new C();
+System.out.println("Subtraction: " + c.sub(10, 5));
+}
+}
